34a2fb5456420babacda08886cee85a24c242abe
Douglas Fuller <dfuller@redhat.com>
2015-07-22 20:59:52 -0400

    mon_client: added support for mon_commands and blacklist_add
    Support sending commands to the Ceph mon for cluster
    maintenance functions. In particular, support 'blacklist_add'
    to prevent other, non-responsive clients from making further
    updates.
    
    Signed-off-by: Douglas Fuller <dfuller@redhat.com>

--- include/linux/ceph/mon_client.h
+++ include/linux/ceph/mon_client.h
@@ -56,6 +56,15 @@ struct ceph_mon_generic_request {
 	struct ceph_msg *reply;    /* and reply */
 };
 
+struct ceph_mon_cmd {
+	struct ceph_mon_request_header monhdr;
+	struct ceph_fsid fsid;
+	/* vector<string> */
+	u32 num_cmds;
+	u32 str_len;
+	char cmd_str[0];
+} __attribute__((packed));
+
 struct ceph_mon_client {
 	struct ceph_client *client;
 	struct ceph_monmap *monmap;
@@ -132,6 +141,8 @@ extern int ceph_monc_open_session(struct ceph_mon_client *monc);
 
 extern int ceph_monc_validate_auth(struct ceph_mon_client *monc);
 
+extern int ceph_monc_blacklist_add(struct ceph_mon_client *monc,
+				   struct ceph_entity_addr *target);
 extern int ceph_monc_create_snapid(struct ceph_mon_client *monc,
 				   u32 pool, u64 *snapid);
 
